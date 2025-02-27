af1c640da11dc85014a3f4c05df3b5a9e664b2bb
Eduardo Tirado Bueno <58276538+etiradob-phys@users.noreply.github.com>
2025-02-27 10:37:14 -0600

    Update NMDB_download_v3.py

--- NMDB_download_v3.py
+++ NMDB_download_v3.py
@@ -171,6 +171,14 @@ dataset_nm_txt['Hour'] = dataset_nm_txt['Datetime'].dt.hour
 dataset_nm_txt['Minute'] = dataset_nm_txt['Datetime'].dt.minute
 dataset_nm_txt['Second'] = dataset_nm_txt['Datetime'].dt.second
 
+dataset_nm_resample_txt = dataset_nm_resample
+dataset_nm_resample_txt = dataset_nm_resample.reset_index()
+
+dataset_nm_resample_txt['Year'] = dataset_nm_resample_txt['Datetime'].dt.year
+
+
+# --------------------------------------------------------------------------------------------------------------------------------------
+
 dataset_nm_txt = dataset_nm_txt[['Datetime', 'Year', 'Month', 'Day', 'Hour', 'Minute', 'Second', 'MCMU', 'THUL', 'INVK', 'NAIN', 'FSMT', 'OULU']]
 dataset_nm_txt.set_index('Datetime', inplace=True)
 print(dataset_nm_txt)
