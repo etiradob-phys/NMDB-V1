9f86a3591a95d085fc08005f0bc931dc198c16da
Eduardo Tirado Bueno <58276538+etiradob-phys@users.noreply.github.com>
2025-02-28 15:37:26 -0600

    Update NMDB_corr_for_efficiency.py

--- NMDB_corr_for_efficiency.py
+++ NMDB_corr_for_efficiency.py
@@ -33,8 +33,8 @@ import scipy
 def nmdb_get(startdate, enddate, station="JUNG"):
     sy,sm,sd = str(startdate).split("-")
     ey,em,ed = str(enddate).split("-")
-    url = "http://nest.nmdb.eu/draw_graph.php?formchk=1&stations[]={station}&tabchoice=1m&dtype=corr_for_pressure&tresolution=1&force=best&yunits=0&date_choice=bydate&start_day={sd}&start_month={sm}&start_year={sy}&start_hour=0&start_min=0&end_day={ed}&end_month={em}&end_year={ey}&end_hour=23&end_min=59&output=ascii&display_null=1"
-#   url = "http://nest.nmdb.eu/draw_graph.php?formchk=1&stations[]={station}&tabchoice=1h&dtype=corr_for_pressure&tresolution=60&force=1&yunits=0&date_choice=bydate&start_day={sd}&start_month={sm}&start_year={sy}&start_hour=0&start_min=0&end_day={ed}&end_month={em}&end_year={ey}&end_hour=23&end_min=59&output=ascii&display_null=1"
+    url = "http://nest.nmdb.eu/draw_graph.php?formchk=1&stations[]={station}&tabchoice=1m&dtype=corr_for_efficiency&tresolution=1&force=best&yunits=0&date_choice=bydate&start_day={sd}&start_month={sm}&start_year={sy}&start_hour=0&start_min=0&end_day={ed}&end_month={em}&end_year={ey}&end_hour=23&end_min=59&output=ascii&display_null=1"
+#   url = "http://nest.nmdb.eu/draw_graph.php?formchk=1&stations[]={station}&tabchoice=1h&dtype=corr_for_efficiency&tresolution=60&force=1&yunits=0&date_choice=bydate&start_day={sd}&start_month={sm}&start_year={sy}&start_hour=0&start_min=0&end_day={ed}&end_month={em}&end_year={ey}&end_hour=23&end_min=59&output=ascii&display_null=1"
     url = url.format(station=station, sd=sd, sm=sm, sy=sy, ed=ed, em=em, ey=ey)
     response = urllib.request.urlopen(url)
     html = response.read()
@@ -79,35 +79,35 @@ tf = f"{year_tf}{month_tf}{day_tf}"
 
 # --------------------------------------------------------------------------------------------------------------------------------------
 
-corr_for_pressure_MCMU = nmdb_get(ti_nmdb, tf_nmdb, "MCMU")     # R = 0.30 GV - STATION 0
-corr_for_pressure_THUL = nmdb_get(ti_nmdb, tf_nmdb, "THUL")     # R = 0.30 GV - STATION 1
-corr_for_pressure_INVK = nmdb_get(ti_nmdb, tf_nmdb, "INVK")     # R = 0.30 GV - STATION 2
-corr_for_pressure_NAIN = nmdb_get(ti_nmdb, tf_nmdb, "NAIN")     # R = 0.30 GV - STATION 3
-corr_for_pressure_FSMT = nmdb_get(ti_nmdb, tf_nmdb, "FSMT")     # R = 0.30 GV - STATION 4
-corr_for_pressure_OULU = nmdb_get(ti_nmdb, tf_nmdb, "OULU")     # R = 0.81 GV - STATION 5
+corr_for_efficiency_MCMU = nmdb_get(ti_nmdb, tf_nmdb, "MCMU")     # R = 0.30 GV - STATION 0
+corr_for_efficiency_THUL = nmdb_get(ti_nmdb, tf_nmdb, "THUL")     # R = 0.30 GV - STATION 1
+corr_for_efficiency_INVK = nmdb_get(ti_nmdb, tf_nmdb, "INVK")     # R = 0.30 GV - STATION 2
+corr_for_efficiency_NAIN = nmdb_get(ti_nmdb, tf_nmdb, "NAIN")     # R = 0.30 GV - STATION 3
+corr_for_efficiency_FSMT = nmdb_get(ti_nmdb, tf_nmdb, "FSMT")     # R = 0.30 GV - STATION 4
+corr_for_efficiency_OULU = nmdb_get(ti_nmdb, tf_nmdb, "OULU")     # R = 0.81 GV - STATION 5
 
-corr_for_pressureMCMU = list(corr_for_pressure_MCMU.items())
-corr_for_pressureTHUL = list(corr_for_pressure_THUL.items())
-corr_for_pressureINVK = list(corr_for_pressure_INVK.items())
-corr_for_pressureNAIN = list(corr_for_pressure_NAIN.items())
-corr_for_pressureFSMT = list(corr_for_pressure_FSMT.items())
-corr_for_pressureOULU = list(corr_for_pressure_OULU.items())
+corr_for_efficiencyMCMU = list(corr_for_efficiency_MCMU.items())
+corr_for_efficiencyTHUL = list(corr_for_efficiency_THUL.items())
+corr_for_efficiencyINVK = list(corr_for_efficiency_INVK.items())
+corr_for_efficiencyNAIN = list(corr_for_efficiency_NAIN.items())
+corr_for_efficiencyFSMT = list(corr_for_efficiency_FSMT.items())
+corr_for_efficiencyOULU = list(corr_for_efficiency_OULU.items())
 
-corr_for_pressure_array_MCMU = np.array(corr_for_pressureMCMU)
-corr_for_pressure_array_THUL = np.array(corr_for_pressureTHUL)
-corr_for_pressure_array_INVK = np.array(corr_for_pressureINVK)
-corr_for_pressure_array_NAIN = np.array(corr_for_pressureNAIN)
-corr_for_pressure_array_FSMT = np.array(corr_for_pressureFSMT)
-corr_for_pressure_array_OULU = np.array(corr_for_pressureOULU)
+corr_for_efficiency_array_MCMU = np.array(corr_for_efficiencyMCMU)
+corr_for_efficiency_array_THUL = np.array(corr_for_efficiencyTHUL)
+corr_for_efficiency_array_INVK = np.array(corr_for_efficiencyINVK)
+corr_for_efficiency_array_NAIN = np.array(corr_for_efficiencyNAIN)
+corr_for_efficiency_array_FSMT = np.array(corr_for_efficiencyFSMT)
+corr_for_efficiency_array_OULU = np.array(corr_for_efficiencyOULU)
 
 # List of NMs and corresponding data arrays
 stations = {
-    'MCMU': corr_for_pressure_array_MCMU,
-    'THUL': corr_for_pressure_array_THUL,
-    'INVK': corr_for_pressure_array_INVK,
-    'NAIN': corr_for_pressure_array_NAIN,
-    'FSMT': corr_for_pressure_array_FSMT,
-    'OULU': corr_for_pressure_array_OULU
+    'MCMU': corr_for_efficiency_array_MCMU,
+    'THUL': corr_for_efficiency_array_THUL,
+    'INVK': corr_for_efficiency_array_INVK,
+    'NAIN': corr_for_efficiency_array_NAIN,
+    'FSMT': corr_for_efficiency_array_FSMT,
+    'OULU': corr_for_efficiency_array_OULU
 }
 
 # Dictionary to store DataFrames
@@ -125,26 +125,26 @@ for name, data in stations.items():
     dfs[name] = df
 
 # Concatenate all DataFrames along the columns
-dataset_nm_corr_for_pressure = pd.concat(dfs.values(), axis=1)
+dataset_nm_corr_for_efficiency = pd.concat(dfs.values(), axis=1)
 
-print(dataset_nm_corr_for_pressure)
+print(dataset_nm_corr_for_efficiency)
 print('---------------------------------------------------------------')
 
 # Extract various components of Datetime:
 
-dataset_nm_corr_for_pressure_txt = dataset_nm_corr_for_pressure
-dataset_nm_corr_for_pressure_txt = dataset_nm_corr_for_pressure.reset_index()
+dataset_nm_corr_for_efficiency_txt = dataset_nm_corr_for_efficiency
+dataset_nm_corr_for_efficiency_txt = dataset_nm_corr_for_efficiency.reset_index()
 
-dataset_nm_corr_for_pressure_txt['Year'] = dataset_nm_corr_for_pressure_txt['Datetime'].dt.year
-dataset_nm_corr_for_pressure_txt['Month'] = dataset_nm_corr_for_pressure_txt['Datetime'].dt.month
-dataset_nm_corr_for_pressure_txt['Day'] = dataset_nm_corr_for_pressure_txt['Datetime'].dt.day
-dataset_nm_corr_for_pressure_txt['Hour'] = dataset_nm_corr_for_pressure_txt['Datetime'].dt.hour
-dataset_nm_corr_for_pressure_txt['Minute'] = dataset_nm_corr_for_pressure_txt['Datetime'].dt.minute
-dataset_nm_corr_for_pressure_txt['Second'] = dataset_nm_corr_for_pressure_txt['Datetime'].dt.second
+dataset_nm_corr_for_efficiency_txt['Year'] = dataset_nm_corr_for_efficiency_txt['Datetime'].dt.year
+dataset_nm_corr_for_efficiency_txt['Month'] = dataset_nm_corr_for_efficiency_txt['Datetime'].dt.month
+dataset_nm_corr_for_efficiency_txt['Day'] = dataset_nm_corr_for_efficiency_txt['Datetime'].dt.day
+dataset_nm_corr_for_efficiency_txt['Hour'] = dataset_nm_corr_for_efficiency_txt['Datetime'].dt.hour
+dataset_nm_corr_for_efficiency_txt['Minute'] = dataset_nm_corr_for_efficiency_txt['Datetime'].dt.minute
+dataset_nm_corr_for_efficiency_txt['Second'] = dataset_nm_corr_for_efficiency_txt['Datetime'].dt.second
 
 # --------------------------------------------------------------------------------------------------------------------------------------
 
-dataset_nm_corr_for_pressure_txt = dataset_nm_corr_for_pressure_txt[['Datetime', 'Year', 'Month', 'Day', 'Hour', 'Minute', 'Second', 'MCMU', 'THUL', 'INVK', 'NAIN', 'FSMT', 'OULU']]
-dataset_nm_corr_for_pressure_txt.set_index('Datetime', inplace=True)
+dataset_nm_corr_for_efficiency_txt = dataset_nm_corr_for_efficiency_txt[['Datetime', 'Year', 'Month', 'Day', 'Hour', 'Minute', 'Second', 'MCMU', 'THUL', 'INVK', 'NAIN', 'FSMT', 'OULU']]
+dataset_nm_corr_for_efficiency_txt.set_index('Datetime', inplace=True)
 
-dataset_nm_corr_for_pressure_txt.to_csv(f'/Users/eduardotiradobueno/Downloads/corr_for_pressure_{ti}to{tf}.txt', header=True, index=None, sep='\t', mode='w')
+dataset_nm_corr_for_efficiency_txt.to_csv(f'/Users/eduardotiradobueno/Downloads/corr_for_efficiency_{ti}to{tf}.txt', header=True, index=None, sep='\t', mode='w')
